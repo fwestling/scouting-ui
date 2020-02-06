0109731a447e4255a4a52a49cba3b42802302b4c
Fred Westling <fredrik.westling@gmail.com>
2020-02-06 22:21:48 +1100

    Updates

---
+++ precache-manifest.f01ed3ce41839904b0b9afff35b9fc68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c130df1fa39d61ccc179dc3fac4ea7f5",
+    "url": "/scouting-ui/index.html"
+  },
+  {
+    "revision": "9e1d2de43abaef8b32c2",
+    "url": "/scouting-ui/static/css/main.c258b918.chunk.css"
+  },
+  {
+    "revision": "6212d171712f5f03818e",
+    "url": "/scouting-ui/static/js/2.92009ef5.chunk.js"
+  },
+  {
+    "revision": "be326db8c8d9b3aa36d942e163e338e7",
+    "url": "/scouting-ui/static/js/2.92009ef5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9e1d2de43abaef8b32c2",
+    "url": "/scouting-ui/static/js/main.59710be1.chunk.js"
+  },
+  {
+    "revision": "b6bc9cedbd3883fb5c2c",
+    "url": "/scouting-ui/static/js/runtime-main.7ff0777d.js"
+  },
+  {
+    "revision": "5f6234b35a6f2cd9ee92a34a0f624d09",
+    "url": "/scouting-ui/static/media/background.5f6234b3.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.b2ccbb3a56aed3bb52a0fa6f204877b9.js"
+  "/scouting-ui/precache-manifest.f01ed3ce41839904b0b9afff35b9fc68.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/scouting-ui/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
